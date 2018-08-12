fdd5377af11053e5728b27324044c3d23addc161
saintz666 <30486634+saintz666@users.noreply.github.com>
2018-08-12 16:33:42 +1200

    Update dirsearch_extract.py

--- dirsearch_extract.py
+++ dirsearch_extract.py
@@ -3,46 +3,47 @@
 Author: SAINTz
 Twitter: @__SAINTz__
 '''
-
 import sys,os
 import argparse
 
 def main(argv):
-	inputfile = ''
-	outputfile = ''
-	parser = argparse.ArgumentParser(description="Parse Dirsearch output and locate HTTP Code 200")
-	parser.add_argument('inputfile', help='The Dirsearch report')
-	parser.add_argument('outputfile', help='The output filename')
-	args = parser.parse_args()
-	inputfile=args.inputfile
-	outputfile = args.outputfile
+    inputfile = ''
+    outputfile = ''
+    parser = argparse.ArgumentParser(description="Parse Dirsearch output and locate HTTP Code 200")
+    parser.add_argument('inputfile', help='The Dirsearch report')
+    parser.add_argument('outputfile', help='The output filename')
+    args = parser.parse_args()
+    inputfile=args.inputfile
+    outputfile = args.outputfile
 
-	http_code_to_view = ['200','500']
-	
-	try:
-		fi = open(inputfile,'r+')
-		fi2 = fi.readlines()
-		fi.close()
-		report = []
-		fo = open(outputfile, 'a+')
-		for x in xrange(len(fi2)):
-			httpcode = ''
-			httpcode = fi2[x].split('   ')[0].strip()
-			if httpcode in http_code_to_view:
-				out = fi2[x].split('   ')[2].strip() + "\n"
-				fo.write(out)
+    http_code_to_view = ['200','500']
 
-		fo.close()
-	except IOError as e:
-		print "IO error({0}): {1}".format(e.errno, e.strerror)
-		sys.exit(2)
-	except:
-		print "Unexpected error:", sys.exc_info()[0]
-		#exc_type, exc_obj, exc_tb = sys.exc_info()
-		#fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-		#print(exc_type, fname, exc_tb.tb_lineno)
-		sys.exit(2)
+    try:
+        fi = open(inputfile,'r+')
+        fi2 = fi.readlines()
+        fi.close()
+        report = []
+        fo = open(outputfile, 'a+')
+        for x in xrange(len(fi2)):
+            httpcode = ''
+            httpcode = fi2[x].split(' ')[0].strip()
+            if httpcode in http_code_to_view:
+                out = fi2[x].split(' ')[-1].strip() + "\n"
+                if len(out) > 10: 
+                    fo.write(out)
 
-	
+        fo.close()
+    except IOError as e:
+        print "IO error({0}): {1}".format(e.errno, e.strerror)
+        sys.exit(2)
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        sys.exit(2)
+
+
 if __name__ == "__main__":
    main(sys.argv)
+
